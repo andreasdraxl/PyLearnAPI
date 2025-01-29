a93c828468d81511b10a80116b295dd190b64bcb
andreasdraxl <andreas_draxl_@live.de>
2025-01-29 10:45:09 +0100

    delete prints

--- LearnRevitAPI.extension/LearnRevitAPI.tab/Homework.panel/BGF.pushbutton/script.py
+++ LearnRevitAPI.extension/LearnRevitAPI.tab/Homework.panel/BGF.pushbutton/script.py
@@ -57,8 +57,7 @@ for item in levels:
     if item.get_Parameter(BuiltInParameter.LEVEL_IS_BUILDING_STORY).AsInteger() == 1:
         matching.append(item)
     
-for i in matching:
-    print(i)
+
     
 
 
