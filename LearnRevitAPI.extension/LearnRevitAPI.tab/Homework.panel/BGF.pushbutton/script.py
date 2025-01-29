d2dc64fa669e4ada6b8e027e77743f37ac1a1546
andreasdraxl <andreas_draxl_@live.de>
2025-01-29 09:09:01 +0100

    new button

---
+++ LearnRevitAPI.extension/LearnRevitAPI.tab/Homework.panel/BGF.pushbutton/script.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+__title__ = "BGF"
+__doc__ = """Version = 1.0
+Date    = 20.04.2022
+Author: Andreas Draxl"""
+
+# Regular + Autodesk
+import os, sys, math, datetime, time                                    # Regular Imports
+from Autodesk.Revit.DB import *                                         # Import everything from DB (Very good for beginners)
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector     # or Import only classes that are used.
+
+# pyRevit
+from pyrevit import revit, forms                                        # import pyRevit modules. (Lots of useful features)
+
+
+# .NET Imports
+import clr                                  # Common Language Runtime. Makes .NET libraries accessinble
+clr.AddReference("System")                  # Refference System.dll for import.
+from System.Collections.Generic import List
+
+# Document reference
+doc   = __revit__.ActiveUIDocument.Document   # Document   class from RevitAPI that represents project. Used to Create, Delete, Modify and Query elements from the project.
+uidoc = __revit__.ActiveUIDocument          # UIDocument class from RevitAPI that represents Revit project opened in the Revit UI.
+app   = __revit__.Application                 # Represents the Autodesk Revit Application, providing access to documents, options and other application wide data and settings.
+PATH_SCRIPT = os.path.dirname(__file__)
+
+
+levels = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements()
+level_names = [i.Name for i in levels]
+
+print(levels)
+print(level_names)
